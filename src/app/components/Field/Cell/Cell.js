53521d3f69ed132187a3ad6c4f6ce5f05bd345e3
rostislav <egregor2011@ya.ru>
2017-03-11 18:56:05 +0300

    small bug fixes

--- src/app/components/Field/Cell/Cell.js
+++ src/app/components/Field/Cell/Cell.js
@@ -5,8 +5,11 @@ import style from './Cell.css';
 const Cell = (props) => {
 
     const _handleClick = () => {
-        let coords = props.coords.toString();
-        return props.action({ship: props.ship, coords: coords, target: props.target})
+        console.log(props);
+        if (props.style === 'default') {
+            let coords = props.coords.toString();
+            return props.action({ship: props.ship, coords: coords, target: props.target})
+        }
     };
 
     return <div key={props.id} onClick={_handleClick} className={style[props.style]}></div>
